a37990ed0c33ee5197959eadd972f51b487b71f5
Kata Gabor <gabor@lipn.univ-paris13.fr>
2019-09-23 18:25:17 +0200

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,21 @@
+import sys
+
+# Ecrivez la fonction 'main' qui ouvre le fichier donné en argument en ligne de commande 
+# (sys.argv[1]) et calcule les fréquences suivantes :
+# 1) la fréquence de chaque mot type dans le corpus : wordfreq
+# 2) la fréquence de chaque bigram type dans le corpus : bifreq
+# 3) la fréquence totale des mots : N
+# 4) la fréquence totale de bigrams : B
+# On considère que les séparateurs de mot sont l'espace et la fin de ligne. 
+
+def main():
+  wordfreq = {}
+  bifreq = {}
+  N = 0
+  B = 0
+ ### your code here ###
+
+
+
+if __name__ == '__main__':
+  main()
